adb5965cc27579c77ba685f70a76e2138ed7d4f6
Veysi ADIN <52937922+veysiadn@users.noreply.github.com>
2021-08-31 10:24:01 +0900

    Update ForceSensorReadingFMA.ino

--- ForceSensorReadingFMA.ino
+++ ForceSensorReadingFMA.ino
@@ -12,8 +12,8 @@
 const int sensor1_select = 10;
 const int sensor2_select = 9;
 // Required for force calculation based on datasheet
-const float output_max = 13107.2; // %80 of 14bit data.
-const float output_min = 3276.8; // %20 of 14 bit data.
+const float output_max = 13107; // %80 of 14bit data.
+const float output_min = 3276; // %20 of 14 bit data.
 
 uint16_t sensor1_force_data=0;
 uint16_t sensor1_temperature_data=0;
@@ -66,10 +66,10 @@ void loop() {
   if(!sensor1_status){
     sensor1_force_data = sensor1_force_data & 0b11111111111111;
     // Conversion of 14 bit force readout to Newton.
-    force_val = ((sensor1_force_data-output_min)*10.0)/(output_max-output_min);
-    //Serial.print("Sensor 1 force value= ");
+    force_val = ((sensor1_force_data-output_min)*15.0)/(output_max-output_min);
+    Serial.print("Sensor 1 force value= ");
     Serial.print(force_val);
-    Serial.println();
+    //Serial.print();
 //    Serial.print("  | Temperature value= ");
 //    Serial.println(temperature_val);
   }
@@ -85,37 +85,36 @@ void loop() {
 // ------------------ First sensor reading ended ------------------------------------------
 // ------------------ Second sensor reading started----------------------------------------
 //
-//  SPI.beginTransaction(MySPISettings);
-//  // Activate chip
-//  digitalWrite(sensor2_select, LOW);
-//  // Read two byte data for force and 2 byte data for temperature.
-//  sensor2_force_data = SPI.transfer16(0);
-//  sensor2_temperature_data = SPI.transfer16(0);
-//  temperature_val2 = (sensor2_temperature_data & 0b1111111111100000) >> 5;
-//  temperature_val2 = (((temperature_val2/2047.0)*200)-50);
-//  // Deactivate sensor
-//  digitalWrite(sensor2_select, HIGH);
-//  // End transaction for second sensor
-//  SPI.endTransaction();
-//  // First 2 bit of 2 byte data is status bit and the rest 14 bits are representing force values.
-//  sensor2_status = (sensor2_force_data & 0b1100000000000000) >> 14;
-//  // if status of sensor is valid print data.
-//  if(!sensor2_status){
-//    sensor2_force_data = sensor2_force_data & 0b11111111111111;
-//    // Conversion of 14 bit force readout to Newton.
-//    force_val2 = ((sensor2_force_data-output_min)*10.0)/(output_max-output_min);
-//    //Serial.print("Sensor 2 force value= ");
-//    Serial.print(force_val2);
-//    Serial.println();
-//    //Serial.print("  | Temperature value= ");
-//   // Serial.println(temperature_val2);
-//  }
-//  else if (sensor1_status==0x01)
-//    Serial.println("Sensor 2 : Device in command mode");
-//  else if (sensor1_status==0x02)
-//    Serial.println("Sensor 2 : Stale (duplicate) data, data has been fetched before.");
-//  else if (sensor1_status==0x03)
-//    Serial.println("Sensor 2 : Diagnostic Condition");
+  SPI.beginTransaction(MySPISettings);
+  // Activate chip
+  digitalWrite(sensor2_select, LOW);
+  // Read two byte data for force and 2 byte data for temperature.
+  sensor2_force_data = SPI.transfer16(0);
+  sensor2_temperature_data = SPI.transfer16(0);
+  temperature_val2 = (sensor2_temperature_data & 0b1111111111100000) >> 5;
+  temperature_val2 = (((temperature_val2/2047.0)*200)-50);
+  // Deactivate sensor
+  digitalWrite(sensor2_select, HIGH);
+  // End transaction for second sensor
+  SPI.endTransaction();
+  // First 2 bit of 2 byte data is status bit and the rest 14 bits are representing force values.
+  sensor2_status = (sensor2_force_data & 0b1100000000000000) >> 14;
+  // if status of sensor is valid print data.
+  if(!sensor2_status){
+    sensor2_force_data = sensor2_force_data & 0b11111111111111;
+    // Conversion of 14 bit force readout to Newton.
+    force_val2 = ((sensor2_force_data-output_min)*15.0)/(output_max-output_min);
+    Serial.print(" Sensor 2 force value= ");
+    Serial.println(force_val2);
+    //Serial.print("  | Temperature value= ");
+   // Serial.println(temperature_val2);
+  }
+  else if (sensor1_status==0x01)
+    Serial.println("Sensor 2 : Device in command mode");
+  else if (sensor1_status==0x02)
+    Serial.println("Sensor 2 : Stale (duplicate) data, data has been fetched before.");
+  else if (sensor1_status==0x03)
+    Serial.println("Sensor 2 : Diagnostic Condition");
 // ---------------------------------- Second sensor reading ended ------------------------------
   delay(100);
 
